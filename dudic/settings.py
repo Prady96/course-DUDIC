64834ea44c0355e968caf575a09dc73dae56751d
From Production <from@praduction.com>
2020-05-20 10:37:37 +0000

    before update

--- dudic/settings.py
+++ dudic/settings.py
@@ -42,10 +42,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
-
-
-
-
 #####################################################################
 
 
